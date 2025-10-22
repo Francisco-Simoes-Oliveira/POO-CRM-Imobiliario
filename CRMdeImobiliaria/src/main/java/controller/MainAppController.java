75261c62799676978601324a65ec8b2d2dda9712
Francisco <simoesdeoliveirafrancisco@mail.com>
2025-10-22 10:16:49 -0300

    feat-sceneBuider*

--- CRMdeImobiliaria/src/main/java/controller/MainAppController.java
+++ CRMdeImobiliaria/src/main/java/controller/MainAppController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -10,10 +12,10 @@ public class MainAppController {
     private TextField campoNome;
 
     @FXML
-    private Button botaoSalvar;
+    private Button Sair;
 
     @FXML
-    private void salvar() {
-        System.out.println("Nome digitado: " + campoNome.getText());
+    private void onSairClick(ActionEvent event) {
+        Platform.exit();
     }
 }
